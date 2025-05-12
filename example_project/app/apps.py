a401488e5151a78169e3dc972541f991a2f6de68
Matti Lamppu <lamppu.matti.akseli@gmail.com>
2025-05-12 18:49:42 +0300

    Add initial version of the project

---
+++ example_project/app/apps.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from django.apps import AppConfig
+
+__all__ = [
+    "ExampleConfig",
+]
+
+
+class ExampleConfig(AppConfig):
+    name = "example_project.app"
